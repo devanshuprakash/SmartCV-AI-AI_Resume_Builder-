ee1bb2498e3d06e19495a7249fb29fd2adfc74fb
Devanshu Prakash <devanshuprakash2301@gmail.com>
2026-01-15 09:15:54 +0530

    ....

---
+++ frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Home from './pages/Home.jsx';
+import Layout from './pages/Layout.jsx';
+import Dashboard from './pages/Dashboard.jsx';
+import ResumeBuilder from './pages/ResumeBuilder.jsx';
+import Preview from './pages/Preview.jsx';
+import Login from './pages/Login.jsx';
+import { Routes,Route } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path='app' element={<Layout />} >
+          <Route index element={<Dashboard />} />
+          <Route path='builder/:resumeId' element={<ResumeBuilder />} />
+      </Route>
+      <Route path='view/:resumeId' element={<Preview />} />
+      <Route path='login' element={<Login />} />
+    </Routes>
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,45 @@
+import { PlusIcon, UploadCloudIcon } from 'lucide-react';
+import React, { useEffect, useState } from 'react'
+import { dummyResumeData } from '../assets/assets';
+
+const Dashboard = () => {
+  const [allResumes,setAllResumes]=useState([])
+  const loadAllResumes = async ()=>{
+    setAllResumes(dummyResumeData)
+  }
+  useEffect(()=>{
+    loadAllResumes(dummyResumeData)
+  },[]);
+  return (
+    <div>
+      <div className="max-w-7xl mx-auto px-4 py-8">
+
+        <p className="text-2xl font-medium mb-6 bg-gradient-to-r from-slate-600 to-slate-700 bg-clip-text text-transparent sm:hidden">
+          Welcome, Joe Doe
+        </p>
+
+        <div className="flex gap-4">
+          <button className="w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600 border border-dashed border-slate-300 group hover:border-indigo-500 hover:shadow-lg transition-all duration-300 cursor-pointer">
+            <PlusIcon className="size-11 transition-all duration-300 p-2.5 bg-gradient-to-br from-indigo-300 to-indigo-500 text-white rounded-full" />
+            <p className="text-sm group-hover:text-indigo-600 transition-all duration-300">
+              Create Resume
+            </p>
+          </button>
+
+          <button className="w-full bg-white sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg gap-2 text-slate-600 border border-dashed border-slate-300 group hover:border-purple-500 hover:shadow-lg transition-all duration-300 cursor-pointer">
+            <UploadCloudIcon className="size-11 transition-all duration-300 p-2.5 bg-gradient-to-br from-purple-300 to-indigo-500 text-white rounded-full" />
+            <p className="text-sm group-hover:text-indigo-600 transition-all duration-300">
+              Upload Existing
+            </p>
+          </button>
+        </div>
+
+        <hr className="border-slate-300 my-6  sm:w-[305px]" />
+
+      </div>
+    </div>
+  )
+}
+
+
+export default Dashboard;
