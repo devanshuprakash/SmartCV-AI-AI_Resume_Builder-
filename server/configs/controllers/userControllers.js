03a763476e65c7b46b9ed4eb1a79f886e59c85cd
Devanshu Prakash <devanshuprakash2301@gmail.com>
2026-01-16 19:55:45 +0530

    fixed some issue

---
+++ server/configs/controllers/userControllers.js
@@ -0,0 +1,13 @@
+ export const registerUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        if(!name || !email || !password){
+            return res.status(400).json({message:"All fields are required"});
+        }
+
+        const user= await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ message: "User already exists" });
+        }
+        const hashedPassword = await bcrypt.hash(password, 10);
+       }
