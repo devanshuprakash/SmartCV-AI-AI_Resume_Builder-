56a283ecbbc39b2d6e7d66d57b97c31bb8afd18c
Devanshu Prakash <devanshuprakash2301@gmail.com>
2026-01-16 23:33:49 +0530

    created controllers

--- server/configs/controllers/userControllers.js
+++ server/configs/controllers/userControllers.js
@@ -1,4 +1,16 @@
- export const registerUser = async (req, res) => {
+import User from "../models/User";
+import bcrypt from "bcrypt";
+
+const generateToken = (userId) => {
+    const token = jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+        expiresIn: '1d',
+    });
+    return token;
+    // Token generation logic (e.g., JWT)
+}
+
+
+export const registerUser = async (req, res) => {
     try {
         const { name, email, password } = req.body;
         if(!name || !email || !password){
@@ -10,4 +22,5 @@
             return res.status(400).json({ message: "User already exists" });
         }
         const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({ name, email, password: hashedPassword });
        }
