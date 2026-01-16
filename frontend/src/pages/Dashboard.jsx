326df182747d6913e914b9c57936b6f97f0b5310
Devanshu Prakash <devanshuprakash2301@gmail.com>
2026-01-17 02:34:33 +0530

    dashboard_frontend

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,14 +1,15 @@
-import { PlusIcon, UploadCloudIcon } from 'lucide-react';
+import { PlusIcon, UploadCloudIcon,FilePenLineIcon, TrashIcon, PencilIcon } from 'lucide-react';
 import React, { useEffect, useState } from 'react'
 import { dummyResumeData } from '../assets/assets';
 
 const Dashboard = () => {
+  const colors = ['#6366F1', '#8B5CF6', '#EC4899', '#F59E0B', '#10B981', '#3B82F6'];
   const [allResumes,setAllResumes]=useState([])
   const loadAllResumes = async ()=>{
     setAllResumes(dummyResumeData)
   }
   useEffect(()=>{
-    loadAllResumes(dummyResumeData)
+    loadAllResumes()
   },[]);
   return (
     <div>
@@ -36,8 +37,31 @@ const Dashboard = () => {
 
         <hr className="border-slate-300 my-6  sm:w-[305px]" />
 
-      </div>
-    </div>
+      <div className="grid grid-cols-2 sm:flex flex-wrap gap-4 ">
+      {allResumes. map((resume, index)=>{
+      const baseColor = colors[index % colors. length];
+      return (
+      <button key={index} className=' relative w-full sm:max-w-36 h-48 flex flex-col items-center justify-center rounded-lg 
+      gap-2 border group hover:shadow-lg transition-all duration-300 cursor-pointer' style= {{background: `linear-gradient(135deg, ${baseColor}10, ${baseColor}40)`,
+      borderColor: baseColor + '40'}} >
+      <FilePenLineIcon className="size-7 group-hover: scale-105
+      transition-all " style={{ color: baseColor }}/>
+      <p className='text-sm group-hover:scale-105 transition-all px-2 text-center' style={{ color: baseColor }}>{resume.title}</p>
+      <p className='absolute bottom-1 text-[11px] text-slate-400 group-hover:text-slate-500 transition-all
+       duration-300 px-2 text-center' style={{ color: baseColor + '90' }}>
+      Updated on {new Date(resume.updatedAt).toLocaleDateString()}
+      </p>
+        <div className='absolute top-1 right-1 group-hover:flex items-center hidden'>
+          <TrashIcon className='size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors'/>
+          <PencilIcon className='size-7 p-1.5 hover:bg-white/50 rounded text-slate-700 transition-colors'/>
+        </div>
+      </button>
+      )
+    })}
+  
+              </div>
+        </div>
+   </div>
   )
 }
 
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { getUserById, loginUser, registerUser } from '../controllers/userControllers';
+import { getUserById, loginUser, registerUser ,getUserResumes} from '../controllers/userControllers';
 import protect from '../middlewares/authMiddleware';
+
 
 const userRouter = express.Router();
 
 userRouter.post('/register',registerUser);
 userRouter.post('/login',loginUser);
 userRouter.gety('/data',protect,getUserById);
-
+userRouter.get('/resumes',protect,getUserResumes);
 export default userRouter;
