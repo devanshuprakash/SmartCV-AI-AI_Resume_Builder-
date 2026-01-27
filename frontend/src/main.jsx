4ff4c93c3fe38add911fdb7da17b61e60eeefe83
devanshuprakash <devanshuprakash2301@gmail.com>
2026-01-27 22:50:24 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import {Provider} from 'react-redux'
 import { store } from './app/store.js'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/app">
+  <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
